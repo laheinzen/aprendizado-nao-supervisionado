12e57a1edf46f4aac34b05305b1447c0e9413b45
Luiz Angelo Heinzen <lah@furb.br>
2021-01-15 16:46:16 -0300

    Apanhando para o Git

--- TrabalhoFinal/Program.cs
+++ TrabalhoFinal/Program.cs
@@ -6,7 +6,7 @@ namespace TrabalhoFinal
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Lutando com o Git!");
         }
     }
 }
