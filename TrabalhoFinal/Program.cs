18cc85cf2d97dad687acc9668af7b0e087a1a996
Luiz Angelo Heinzen <lah@furb.br>
2021-01-15 16:43:34 -0300

    Projeto Inicial VisualStudio

---
+++ TrabalhoFinal/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TrabalhoFinal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
